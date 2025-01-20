a46a0add3057ebe799badc58bfe836eb8af88979
AMMRUTAA <amrutaaushikar04@gmail.com>
2025-01-20 11:30:33 +0530

    my new python program

---
+++ type2.py
@@ -0,0 +1,12 @@
+# Python code to demonstrate Tpye coversion
+# using ord(),hex(),oct()
+#initializing integer
+s = '4'
+#printing character converting to integer
+c = ord(s)
+print ("After  converting 56 to hexadecimal string : ",end="")
+print (c)
+#printing integer converting to octal string
+c = oct(56)
+print  ("After converting 56 to octal string :",end="")
+print (c)
