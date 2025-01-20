1afd55bc265b56067a86a8734c5d4c9ee68e51e5
AMMRUTAA <amrutaaushikar04@gmail.com>
2025-01-20 12:00:32 +0530

    adding new python file

---
+++ type4.py
@@ -0,0 +1,16 @@
+# Python code to demonstrate Type conversion
+# using dict(),complex(),str()
+
+# intializing integers
+a = 1
+b = 2
+# initisalizing tuple
+tup = (('a',1),('f',2),('g',3))
+# printing integer converting to complex number
+c = complex(1,2)
+print ("After converting integr to string :",end="")
+print(c)
+# printing tuple converting to expression dictionary
+c = dict (tup)
+print ("After converting tuple to dictionary : ",end="")
+print (c)
