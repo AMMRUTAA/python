a53964d578b531fc3f20d406a9df4e902fbaa7fb
AMMRUTAA <amrutaaushikar04@gmail.com>
2025-01-20 11:47:32 +0530

    adding new python file

---
+++ type3.py
@@ -0,0 +1,18 @@
+# Pyton code to demonstrate Tpye conversion
+# using tuple(),set(),list()
+
+# initializing string
+s = 'geeks'
+# printing string converting to tuple
+c = tuple(s)
+print ("After converting string to tuple :",end="")
+print(c)
+# printing string converting to set
+c = set(s)
+print ("After converting string to set :",end="")
+print (c)
+# printing string converting to list
+c = list(s)
+print ("After converting string to list : ",end="")
+print(c)       
+        
