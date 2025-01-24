12f7bd4297fb7009bdbd77513a72387fc453b823
AMMRUTAA <amrutaaushikar04@gmail.com>
2025-01-24 09:44:12 +0530

    first commit

---
+++ for loop.py
@@ -0,0 +1,54 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================================ RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/Assisgnmentoperatos.py ===========================================
+1. Addition Value is 35
+2. Addition Value is  55
+Subtraction Value is  35
+Subtraction Value is  700
+Subtraction Value is  35.0
+Modulas Value is:  2
+Modulas Value is:  1048576
+Modulas Value is:  52428
+>>> 
+============================================ RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/comparisionoperator.py ===========================================
+1 a and b are not equal
+2. a is greater than b
+3. a is not less than b
+3. a is not equal to b
+>>> 
+============================================== RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/identyoperators.py =============================================
+1 a= 30 : 140736484673352 b= 30 : 140736484673352
+a and b have same identity
+a and b have same identity
+a= 30 : 140736484673352 b= 40 : 140736484673672
+a and b do not have same identity
+>>> 
+============================================ RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/memebershipoperator.py ===========================================
+1 a and b are not equal
+2. a is greater than b
+3. a is not less than b
+3. a is not equal to b
+>>> 
+============================================= RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/bitwise operation.py ============================================
+a= 63 : 0b111111 b = 13 : 0b1101
+Result of bitwise AND is  13 : 0b1101
+Result of bitwise OR is  63 : 0b111111
+Result of bitwise XOR is  50 : 0b110010
+Result of complete is  -64 : -0b1000000
+Result of left shift is 252 : 0b11111100
+Result of Right shift is  15 : 0b1111
+>>> 
+================================================= RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/nestedif.py =================================================
+Enter the Number:2
+Number is not divisible by 2 and 3
+>>> 3
+3
+>>> 
+================================================== RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/ifelse.py ==================================================
+Enter the Number:2
+Number is divisible by 2
+>>> 
+=================================================== RESTART: C:/Users/amrutaaushikar/AppData/Local/Programs/Python/Python313/elif.py ===================================================
+Enter the Amount:1000
+Discount for amount < 5000 is 200.0
